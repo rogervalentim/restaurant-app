09a0574f8eeda045733d134a7701522c6c8d1de9
rogervalentim <valentim.roger33@gmail.com>
2022-11-24 00:06:15 -0300

    initial commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+import './Header.css';
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dinning</h1>
+    </div>
+  </div>
+);
+
+export default Header;
